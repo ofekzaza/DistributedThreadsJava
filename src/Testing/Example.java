d4f1fd86b4f8421a73aa0796b7ea93cf382303c0
ofekzaza <ofekeshet@gmail.com>
2018-12-25 16:49:02 +0200

    dffdsfdssfd

---
+++ src/Testing/Example.java
@@ -0,0 +1,36 @@
+package Testing;
+
+import GsonInformation.Gsons;
+import Master.CloseType;
+import Master.MasterDThread;
+
+public class Example extends MasterDThread {
+    private MasterDThread masterDThread;
+
+    public static void main(String[] args){
+        Example example = new Example();
+        example.start();
+    }
+
+    @Override
+    public void run(){
+        String[] sources = {""};
+        String[] dependencies = {""};
+        Gsons.Basic basic = new Gsons.Basic("value");
+        masterDThread.execute("Example", "Distributed/Example", sources, dependencies, Gsons.gson.toJson(basic));
+
+        masterDThread.waitForResults();
+
+        System.out.println(masterDThread.getAnswer("Example"));
+    }
+
+    @Override
+    public void start(){
+        if(masterDThread == null){
+            masterDThread = new MasterDThread(this, CloseType.Kill);
+            masterDThread.start();
+        }
+    }
+
+}
+
